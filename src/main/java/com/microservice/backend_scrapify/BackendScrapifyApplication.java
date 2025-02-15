0437893b522a9557ef06359b71aa856c60c5945e
Satish Patil <syapatil0803@gmail.com>
2025-02-15 23:19:22 +0530

    Initial Commit

---
+++ src/main/java/com/microservice/backend_scrapify/BackendScrapifyApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.backend_scrapify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendScrapifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendScrapifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/backend_scrapify/BackendScrapifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.backend_scrapify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendScrapifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
